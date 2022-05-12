d517a5b5ca1210cd8d18709c77b30f60b7223a7d
Fastion-1024 <dsprice1989@outlook.com>
2022-05-12 15:17:26 +0100

    Added Pages - Split app into two pages. - Home page will show recent locations and the search bar - WeeklyForecast page will display a forecast along with the search bar.

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import LocationSearch from '../../components/LocationSearch/LocationSearch';
+
+const Home = () => {
+    return (
+        <main>
+            <LocationSearch />
+        </main>
+    );
+};
+
+export default Home;
---
+++ src/pages/WeeklyForecast/WeeklyForecast.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WeeklyForecast = () => {
+    return <main>WeeklyForecast</main>;
+};
+
+export default WeeklyForecast;
