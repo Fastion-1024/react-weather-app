05809a5f3085eb6cbc97d97fbfd7f0a638f07c12
Fastion-1024 <dsprice1989@outlook.com>
2022-05-12 15:18:48 +0100

    Refactored to Use New Pages Components - Conditionally renders either page component based on AppProvider state. - Removed styling except for headers

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,10 @@
-import SearchBar from './SearchBar/SearchBar';
-import WeatherContainer from './WeatherContainer/WeatherContainer';
 import { useAppContext } from './AppProvider';
+import { Home, WeeklyForecast } from '../pages';
 import './App.css';
 
 function App() {
-    return (
-        <main>
-            <SearchBar />
-            <WeatherContainer />
-        </main>
-    );
+    const { weeklyForecast } = useAppContext();
+    return <>{!weeklyForecast ? <Home /> : <WeeklyForecast />}</>;
 }
 
 export default App;
